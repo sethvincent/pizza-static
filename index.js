237fc4ab0d982529d3110a4d9029949ffb9ac805
sethvincent <sethvincent@gmail.com>
2016-06-24 12:03:26 -0700

    Updates

---
+++ index.js
@@ -0,0 +1,7 @@
+
+  var insertCSS = require('insert-css')
+  var css = require('sheetify')
+  var minidocs = require('minidocs')({"title":"pizza-static","contents":{"Welcome!":"pizza.md","More pizza":"more-pizza.md","Bestest":"best-pizza.md"},"markdown":{"best-pizza":"# woo pizza\n\nhello best pizza is any pizza\n","more-pizza":"# more\n\nhello more pizza please\n","pizza":"# pizza\n\nwelcome to pizza\n"},"initial":"best-pizza","basedir":"/pizza-static"})
+  undefined
+  minidocs.start('#choo-root')
+  
