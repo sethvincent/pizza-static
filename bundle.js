d303f8184e86abd7f6803061c9095162516df6cc
sethvincent <sethvincent@gmail.com>
2016-06-24 15:15:13 -0700

    Updates

--- bundle.js
+++ bundle.js
@@ -131,8 +131,8 @@ module.exports = function (params, state, send) {
   }
 
   function isActive (current, item) {
-    console.log('current', current)
-    console.log('item', item)
+    console.log('current', current, current.length)
+    console.log('item', item, item.length)
     return current === item ? 'active' : ''
   }
 
